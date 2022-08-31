7b3e287af3e6792a362af436c8b496a8827b12e1
unknown <khoado.dk@gmail.com>
2022-08-30 22:00:43 -0400

    Woocommerce set up

---
+++ header.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * The header for our theme
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package colorme
+ */
+
+?>
+<!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="https://gmpg.org/xfn/11">
+
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+<?php wp_body_open(); ?>
+<div id="page" class="site">
+	<header id="masthead" class="site-header flex " style="<?php if( !is_front_page() OR !is_home()){category_header_background();} ?>">
+		<nav id="site-navigation" class="main-navigation flex flex-auto items-end">
+			<!-- Logo -->
+			<a href="<?php echo get_site_url() ?>" class="logo">
+				<h1>Color Me</h1>
+			</a>
+			<!-- Nav -->
+			<?php
+			wp_nav_menu(
+				array(
+					'theme_location' => 'menu-1',
+					'menu_id'        => 'primary-menu',
+					'menu_class' => 'main-menu ma0 pa0 list'
+				)
+			);
+			?>
+		</nav><!-- #site-navigation -->
+
+		<div class="site-branding">
+			<?php echo wp_get_attachment_image(14, 'full'); ?>
+		</div>
+	</header><!-- #masthead -->
---
+++ inc/woocommerce.php
@@ -0,0 +1,224 @@
+<?php
+/**
+ * WooCommerce Compatibility File
+ *
+ * @link https://woocommerce.com/
+ *
+ * @package colorme
+ */
+
+/**
+ * WooCommerce setup function.
+ *
+ * @link https://docs.woocommerce.com/document/third-party-custom-theme-compatibility/
+ * @link https://github.com/woocommerce/woocommerce/wiki/Enabling-product-gallery-features-(zoom,-swipe,-lightbox)
+ * @link https://github.com/woocommerce/woocommerce/wiki/Declaring-WooCommerce-support-in-themes
+ *
+ * @return void
+ */
+function colorme_woocommerce_setup() {
+	add_theme_support(
+		'woocommerce',
+		array(
+			'thumbnail_image_width' => 150,
+			'single_image_width'    => 300,
+			'product_grid'          => array(
+				'default_rows'    => 3,
+				'min_rows'        => 1,
+				'default_columns' => 4,
+				'min_columns'     => 1,
+				'max_columns'     => 6,
+			),
+		)
+	);
+	add_theme_support( 'wc-product-gallery-zoom' );
+	add_theme_support( 'wc-product-gallery-lightbox' );
+	add_theme_support( 'wc-product-gallery-slider' );
+}
+add_action( 'after_setup_theme', 'colorme_woocommerce_setup' );
+
+
+/**
+ * Disable the default WooCommerce stylesheet.
+ *
+ * Removing the default WooCommerce stylesheet and enqueing your own will
+ * protect you during WooCommerce core updates.
+ *
+ * @link https://docs.woocommerce.com/document/disable-the-default-stylesheet/
+ */
+add_filter( 'woocommerce_enqueue_styles', '__return_empty_array' );
+
+/**
+ * Add 'woocommerce-active' class to the body tag.
+ *
+ * @param  array $classes CSS classes applied to the body tag.
+ * @return array $classes modified to include 'woocommerce-active' class.
+ */
+function colorme_woocommerce_active_body_class( $classes ) {
+	$classes[] = 'woocommerce-active';
+
+	return $classes;
+}
+add_filter( 'body_class', 'colorme_woocommerce_active_body_class' );
+
+/**
+ * Related Products Args.
+ *
+ * @param array $args related products args.
+ * @return array $args related products args.
+ */
+function colorme_woocommerce_related_products_args( $args ) {
+	$defaults = array(
+		'posts_per_page' => 3,
+		'columns'        => 3,
+	);
+
+	$args = wp_parse_args( $defaults, $args );
+
+	return $args;
+}
+add_filter( 'woocommerce_output_related_products_args', 'colorme_woocommerce_related_products_args' );
+
+/**
+ * Remove default WooCommerce wrapper.
+ */
+remove_action( 'woocommerce_before_main_content', 'woocommerce_output_content_wrapper', 10 );
+remove_action( 'woocommerce_after_main_content', 'woocommerce_output_content_wrapper_end', 10 );
+
+if ( ! function_exists( 'colorme_woocommerce_wrapper_before' ) ) {
+	/**
+	 * Before Content.
+	 *
+	 * Wraps all WooCommerce content in wrappers which match the theme markup.
+	 *
+	 * @return void
+	 */
+	function colorme_woocommerce_wrapper_before() {
+		?>
+			<main id="primary" class="site-main">
+		<?php
+	}
+}
+add_action( 'woocommerce_before_main_content', 'colorme_woocommerce_wrapper_before' );
+
+if ( ! function_exists( 'colorme_woocommerce_wrapper_after' ) ) {
+	/**
+	 * After Content.
+	 *
+	 * Closes the wrapping divs.
+	 *
+	 * @return void
+	 */
+	function colorme_woocommerce_wrapper_after() {
+		?>
+			</main><!-- #main -->
+		<?php
+	}
+}
+add_action( 'woocommerce_after_main_content', 'colorme_woocommerce_wrapper_after' );
+
+/**
+ * Sample implementation of the WooCommerce Mini Cart.
+ *
+ * You can add the WooCommerce Mini Cart to header.php like so ...
+ *
+	<?php
+		if ( function_exists( 'colorme_woocommerce_header_cart' ) ) {
+			colorme_woocommerce_header_cart();
+		}
+	?>
+ */
+
+if ( ! function_exists( 'colorme_woocommerce_cart_link_fragment' ) ) {
+	/**
+	 * Cart Fragments.
+	 *
+	 * Ensure cart contents update when products are added to the cart via AJAX.
+	 *
+	 * @param array $fragments Fragments to refresh via AJAX.
+	 * @return array Fragments to refresh via AJAX.
+	 */
+	function colorme_woocommerce_cart_link_fragment( $fragments ) {
+		ob_start();
+		colorme_woocommerce_cart_link();
+		$fragments['a.cart-contents'] = ob_get_clean();
+
+		return $fragments;
+	}
+}
+add_filter( 'woocommerce_add_to_cart_fragments', 'colorme_woocommerce_cart_link_fragment' );
+
+if ( ! function_exists( 'colorme_woocommerce_cart_link' ) ) {
+	/**
+	 * Cart Link.
+	 *
+	 * Displayed a link to the cart including the number of items present and the cart total.
+	 *
+	 * @return void
+	 */
+	function colorme_woocommerce_cart_link() {
+		?>
+		<a class="cart-contents" href="<?php echo esc_url( wc_get_cart_url() ); ?>" title="<?php esc_attr_e( 'View your shopping cart', 'colorme' ); ?>">
+			<?php
+			$item_count_text = sprintf(
+				/* translators: number of items in the mini cart. */
+				_n( '%d item', '%d items', WC()->cart->get_cart_contents_count(), 'colorme' ),
+				WC()->cart->get_cart_contents_count()
+			);
+			?>
+			<span class="amount"><?php echo wp_kses_data( WC()->cart->get_cart_subtotal() ); ?></span> <span class="count"><?php echo esc_html( $item_count_text ); ?></span>
+		</a>
+		<?php
+	}
+}
+
+if ( ! function_exists( 'colorme_woocommerce_header_cart' ) ) {
+	/**
+	 * Display Header Cart.
+	 *
+	 * @return void
+	 */
+	function colorme_woocommerce_header_cart() {
+		if ( is_cart() ) {
+			$class = 'current-menu-item';
+		} else {
+			$class = '';
+		}
+		?>
+		<ul id="site-header-cart" class="site-header-cart">
+			<li class="<?php echo esc_attr( $class ); ?>">
+				<?php colorme_woocommerce_cart_link(); ?>
+			</li>
+			<li>
+				<?php
+				$instance = array(
+					'title' => '',
+				);
+
+				the_widget( 'WC_Widget_Cart', $instance );
+				?>
+			</li>
+		</ul>
+		<?php
+	}
+}
+
+/**
+ * Find current category
+ */
+if ( ! function_exists( 'get_category_id' ) ) {
+	function get_category_id() {
+		$category = get_queried_object();
+		return $category-> term_id;
+	}
+}
+/**
+ * return background color according to the category
+ */
+if ( ! function_exists( 'category_header_background' ) ) {
+	function category_header_background() {
+		$category_id = get_category_id();
+		$bg_color = get_field('background_color', 'product_cat_' . $category_id);
+		echo 'background-color: ' . $bg_color;
+	}
+}
